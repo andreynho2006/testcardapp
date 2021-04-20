fa4698f24728f70355b77cb6f155a3bbce24bbc2
Andrei Cirlan <andreynho2006@gmail.com>
2021-04-20 17:54:09 +0300

    Initial Commit

---
+++ TestCardsApp/TestCardsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCardsAppApp.swift
+//  TestCardsApp
+//
+//  Created by Andrei Cirlan on 20.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct TestCardsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
