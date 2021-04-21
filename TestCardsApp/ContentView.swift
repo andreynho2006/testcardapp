d98564a0744cf750bccb8a7d00311745fd4e03ea
Andrei Cirlan <andreynho2006@gmail.com>
2021-04-21 18:03:16 +0300

    initialise the project

--- TestCardsApp/ContentView.swift
+++ TestCardsApp/ContentView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+            
+        Text("Hello")
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.orange/*@END_MENU_TOKEN@*/)
+            
     }
 }
 
