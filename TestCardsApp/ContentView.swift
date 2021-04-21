86b8eb3c7b78db0705cbd3f06fbceec5bc1fe2ea
Andrei Cirlan <andreynho2006@gmail.com>
2021-04-21 21:51:49 +0300

    make the UI

--- TestCardsApp/ContentView.swift
+++ TestCardsApp/ContentView.swift
@@ -9,23 +9,55 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        
+        ZStack{
             Spacer()
-            
-            Image("logo")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-            Spacer()
-            HStack{
+            VStack{
                 Spacer()
-                Text("Hello")
+                Image("logo")
                 Spacer()
-                Text("World")
+                HStack{
+                    Spacer()
+                    Image("card-2")
+                    Spacer()
+                    Image("card-3")
+                    Spacer()
+                }
+                Spacer()
+                Button(action: {
+                    print("Click")
+                }, label: {
+                    Image("deal-button")
+                })
+                
+                Spacer()
+                HStack{
+                    Spacer()
+                    VStack{
+                        Text("Player")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        Text("0")
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                    VStack{
+                        Text("CPU")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        Text("0")
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                }
                 Spacer()
             }
             
-            Spacer()
-        }
+        }.background(Color(hue: 0.363, saturation: 0.878, brightness: 0.411)).ignoresSafeArea()
     }
 }
 
